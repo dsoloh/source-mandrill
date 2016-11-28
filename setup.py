2e45a521b275fcf2b7502524a710d23e00409fac
kfir <kfirgez@gmail.com>
2016-11-28 11:12:22 +0200

    use mandrill sdk (pip)

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from distutils.core import setup
 
 setup(
     name="panoply_mandrill",
-    version="0.1",
+    version="0.2",
     description="Panoply Data Source for Mandrill API",
-    author="Oshri Bienhaker",
-    author_email="oshri@panoply.io",
+    author="Oshri Bienhaker, Kfir Gez",
+    author_email="oshri@panoply.io, kfir@panoply.io",
     url="http://panoply.io",
     package_dir={"panoply": ""},
     install_requires=[
         "panoply-python-sdk",
+        "mandrill==1.0.57",
         "mock==1.0.1"
     ],
     packages=["panoply.mandrill"]
