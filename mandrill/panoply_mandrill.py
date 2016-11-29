42c70f25f5ba72d2d27804ecd47a4635e4bca79f
kfir <kfirgez@gmail.com>
2016-11-29 11:23:24 +0200

    compact code

--- mandrill/panoply_mandrill.py
+++ mandrill/panoply_mandrill.py
@@ -19,11 +19,8 @@ class PanoplyMandrill(panoply.DataSource):
     def __init__(self, source, opt):
         super(PanoplyMandrill, self).__init__(source, opt)
 
-        if "destination" not in source:
-            source["destination"] = DESTINATION
-
-        if "idpattern" not in source:
-            source["idpattern"] = IDPATTERN
+        source["destination"] = source.get("destination") or DESTINATION
+        source["idpattern"] = source.get("idpattern") or IDPATTERN
 
         fromsec = int(time.time() - (DAY_RANGE * DAY))
         self.fromTime = time.strftime("%Y-%m-%d", time.gmtime(fromsec))
@@ -39,11 +36,13 @@ class PanoplyMandrill(panoply.DataSource):
         if len(self.metrics) == 0:
             return None # No more data to consume
         metric = self.metrics[0]
+
         # for now lets skip the required stuff
         while metric.get('required'):
             metric = self.metrics[0]
             self.metrics.pop(0)
-        result = getattr(getattr(self.mandrill_client, metric['category']), metric['path'])()
+
+        result = self.getFn(metric)()
         for row in result:
             row["type"] = metric["name"]
             row["key"] = self.key
@@ -55,3 +54,7 @@ class PanoplyMandrill(panoply.DataSource):
         loaded = self.total - len(self.metrics)
         msg = "%s of %s metrics loaded" % (loaded, self.total)
         self.progress(loaded, self.total, msg)
+    
+    def getFn(self, metric):
+        # dynamically locate the right function to call from the sdk.
+        return getattr(getattr(self.mandrill_client, metric['category']), metric['path'])
