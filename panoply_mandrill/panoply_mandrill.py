8fc2d065c351950864a75587394a04a824b03640
kfir <kfirgez@gmail.com>
2016-12-04 13:17:45 +0200

    return the shuttle copyfileobj

--- panoply_mandrill/panoply_mandrill.py
+++ panoply_mandrill/panoply_mandrill.py
@@ -142,6 +142,8 @@ class PanoplyMandrill(panoply.DataSource):
         
         # now we have the url to download from
         req = urlopen(url)
+        with open('testtest.zip', 'wb') as fp:
+            shutil.copyfileobj(req, fp, COPY_CHUNK_SIZE)
         zf = zipfile.ZipFile(req)
         csv_reader = csv.DictReader(zf.open('activity.csv'), delimiter=',')
         count = 0
