76d53520936fdc6f89f8018a93a55149291b6008
kfir <kfirgez@gmail.com>
2017-01-31 12:03:08 +0200

    add missing if

--- panoply_mandrill/panoply_mandrill.py
+++ panoply_mandrill/panoply_mandrill.py
@@ -91,6 +91,7 @@ class PanoplyMandrill(panoply.DataSource):
         key = ''
         key += self.key + '-'
         for field in EXPORT_COUNTER_KEY_FIELDS:
+            if field in row:
                 key += row[field].decode('utf-8')
             key += '-'
         key = key[:-1] # remove the last '-'
