d558e0c1963c41f1f5f191d7cabd2516f2bc4684
Offir Baron <Offir@Offirs-MacBook-Pro.local>
2017-10-01 17:54:24 +0300

    merge with branch memory-issues

--- setup.py
+++ setup.py
@@ -11,9 +11,15 @@ setup(
     install_requires=[
         "requests==2.3.0",
         "panoply-python-sdk",
-        "mock==1.0.1",
-        "mandrill==1.0.57",
-        "csvsort==1.3"
+        "csvsort==1.3",
+        "mandrill==1.0.57"
     ],
+    extras_require={
+        "test": [
+            "pep8==1.7.0",
+            "coverage==4.3.4",
+            "mock==1.0.1",
+        ]
+    },
     packages=["panoply.panoply_mandrill"]
 )
