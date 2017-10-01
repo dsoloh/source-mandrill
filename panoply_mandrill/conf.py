8b419522e7edf25615ee69a4cb4d167e26f15a2f
Offir Baron <Offir@Offirs-MacBook-Pro.local>
2017-10-01 18:23:52 +0300

    pep8 fixes

--- panoply_mandrill/conf.py
+++ panoply_mandrill/conf.py
@@ -1,48 +1,48 @@
-DAY_RANGE = 30 # The range of days to import data from
+DAY_RANGE = 30  # The range of days to import data from
 
 metrics = [
     {
-        "name":"messages",
-        "path":"search_time_series",
+        "name": "messages",
+        "path": "search_time_series",
         "includeTimeframe": True
     },
 
     {
-        "name":"tags",
-        "path":"all_time_series"
+        "name": "tags",
+        "path": "all_time_series"
     },
 
     {
-        "name":"senders",
-        "path":"time_series",
-        "required":"address",
+        "name": "senders",
+        "path": "time_series",
+        "required": "address",
     },
 
     {
-        "name":"urls",
-        "path":"time_series",
-        "required":"url",
+        "name": "urls",
+        "path": "time_series",
+        "required": "url",
     },
 
     {
-        "name":"templates",
-        "path":"time_series",
-        "required":"name",
+        "name": "templates",
+        "path": "time_series",
+        "required": "name",
     },
 
     {
-        "name":"webhooks",
-        "path":"list"
+        "name": "webhooks",
+        "path": "list"
     },
 
     {
-        "name":"subaccounts",
-        "path":"list"
+        "name": "subaccounts",
+        "path": "list"
     },
     # it is preferable to leave the exports metric as the last metric
     # since it can take a long time for it to resolve
     {
-        "name":"exports",
+        "name": "exports",
         "path": "activity",
         "includeTimeframe": True
     }
